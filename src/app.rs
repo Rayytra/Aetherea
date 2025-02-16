a3c17d2a0ed658b597f50dd84077e83abb6b553a
Rayytra <rayytra@gmail.com>
2025-02-16 22:46:11 +0700

    egui rewrite (for integration purposes)

---
+++ src/app.rs
@@ -0,0 +1,56 @@
+use eframe::*;
+use egui::{menu, CentralPanel, TopBottomPanel};
+
+pub struct AethereaApp;
+
+impl eframe::App for AethereaApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        CentralPanel::default().show(ctx, |_ui| {
+            TopBottomPanel::top("menubar").show(ctx, |ui| {
+                menu::bar(ui, |ui| {
+                    ui.menu_button("File", |ui| {
+                        if ui.button("New").clicked() {
+                            println!("New File created.")
+                        }
+
+                        if ui.button("Open").clicked() {
+                            println!("Opened File.")
+                        }
+
+                        if ui.button("Open Recent").clicked() {
+                            println!("Opening Recent File.")
+                        }
+
+                        if ui.button("Save").clicked() {
+                            println!("Saved.")
+                        }
+
+                        if ui.button("Save As").clicked() {
+                            println!("Saved As ...")
+                        }
+                    });
+
+                    ui.menu_button("Edit", |ui| {
+                        if ui.button("Undo").clicked() {
+                            println!("Undone.")
+                        }
+
+                        if ui.button("Redo").clicked() {
+                            println!("Redone.")
+                        }
+
+                        if ui.button("Preferences").clicked() {
+                            println!("Opening Preferences.")
+                        }
+                    });
+
+                    ui.menu_button("View", |ui| {
+                        if ui.button("Reset").clicked() {
+                            println!("Reset.")
+                        }
+                    });
+                })
+            })
+        });
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,21 @@
-use core::time;
-use slint::Timer;
-use slint::LogicalPosition;
-use slint::LogicalSize;
-use rdev::display_size;
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+mod app;
 
-slint::include_modules!();
+use eframe::{run_native, NativeOptions};
+use app::AethereaApp;
 
-fn main() {
-    let (screen_width, screen_height) = display_size().unwrap();
-    let splashscreen = SplashScreen::new().unwrap();
-    let mainwindow = MainWindow::new().unwrap();
-
-    const VERSION: &str = env!("CARGO_PKG_VERSION");
-    splashscreen.set_version(format!("ver {}", VERSION).into());
-
-    let splashscreen_weak = splashscreen.as_weak();
-    let mainwindow_weak = mainwindow.as_weak();
-
-    mainwindow.window().set_size(LogicalSize::new((screen_width - 86) as f32, (screen_height - 98) as f32));
-    center_window(&splashscreen.window());
-    center_window(&mainwindow.window());
-
-    splashscreen.show().unwrap();
-
-    Timer::single_shot(time::Duration::from_secs(2), move || {
-        
-        if let Some(splash) = splashscreen_weak.upgrade() {
-            splash.hide().unwrap(); //GET OUTTT
-        }
-        
-        if let Some(main) = mainwindow_weak.upgrade() {
-            main.show().unwrap(); //Coming out
-        }
-    });
-
-    slint::run_event_loop().unwrap();
-}
-
-fn center_window(window: &slint::Window) {
-    let (screen_width, screen_height) = display_size().unwrap();
+fn main() -> eframe::Result<(), eframe::Error> {
+    let native_options = NativeOptions {
+        centered: true,
+        ..Default::default()
+    };
     
-    let window_width = window.size().width as f32;
-    let window_height = window.size().height as f32;
-
-    let center_x = (screen_width as f32 - window_width) / 2.0;
-    let center_y = (screen_height as f32 - window_height) / 2.0;
-
-    window.set_position(LogicalPosition::new(center_x, center_y));
+    let _ = run_native(
+        "Aetherea",
+        native_options,
+        Box::new(|_cc| {
+            Ok(Box::new(AethereaApp))
+        })
+    );
+    Ok(())
 }
